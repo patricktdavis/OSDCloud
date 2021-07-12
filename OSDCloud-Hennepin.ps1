9f5f552d26dd4444b7c918751c9800a4644c75b7
patricktdavis <patricktdavis@outlook.com>
2021-07-12 13:51:43 -0500

    Add files via upload

---
+++ OSDCloud-Hennepin.ps1
@@ -0,0 +1,95 @@
+#=============================================================================
+#region SCRIPT DETAILS
+#=============================================================================
+
+<#
+.SYNOPSIS
+Runs a menu on OSDCloud allowing techs to deploy a machine with Chosen Enterprise image
+
+.EXAMPLE
+PS C:\> OSDCloud-Hennepin.ps1
+#>
+
+#=============================================================================
+#endregion
+#=============================================================================
+#region VARIABLES
+#=============================================================================
+
+
+
+#=============================================================================
+#endregion
+#=============================================================================
+#region FUNCTIONS
+#=============================================================================
+
+function Invoke-OSDCloud {
+    <#
+    .SYNOPSIS
+    Synopsis
+
+    .EXAMPLE
+    Invoke-OSDCloud
+
+    .INPUTS
+    None
+    You cannot pipe objects to Invoke-OSDCloud.
+
+    .OUTPUTS
+    None
+    The cmdlet does not return any output.
+    #>
+
+    [CmdletBinding()]
+    Param()
+
+    #Change Display Resolution for Virtual Machine
+    if ((Get-MyComputerModel) -match 'Virtual') {
+        Write-Host -ForegroundColor Cyan 'Setting Display Resolution to 1600x'
+        Set-DisRes 1600
+    }
+
+    Write-Host -ForegroundColor Cyan "Starting Hennepin County's Custom OSDCloud ..."
+    Start-Sleep -Seconds 5
+    Clear-Host
+    Write-Host '===================== Main Menu =======================' -ForegroundColor Yellow
+    Write-Host '================ Hennepin County SSD =================' -ForegroundColor Yellow
+    Write-Host '== Please Put in a Assyst ticket if there are any issues ==' -ForegroundColor Yellow
+    Write-Host '=======================================================' -ForegroundColor Yellow
+    Write-Host '1: Zero-Touch Win10 20H2 | English | Enterprise'-ForegroundColor Yellow
+    Write-Host "2: Exit`n"-ForegroundColor Yellow
+    $OSDinput = Read-Host 'Please make a selection'
+
+    #Make sure I have the latest OSD Content
+    Write-Host -ForegroundColor Cyan 'Updating OSD PowerShell Module'
+    Install-Module OSD -Force
+
+    Write-Host -ForegroundColor Cyan 'Importing OSD PowerShell Module'
+    Import-Module OSD -Force
+
+    #Start OSDCloud ZTI the RIGHT way
+    Write-Host -ForegroundColor Cyan 'Start OSDCloud with MY Parameters'
+    switch ($OSDinput) {
+        '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI }
+        '2' { Exit }
+    }
+
+}
+
+
+#=============================================================================
+#endregion
+#=============================================================================
+#region EXECUTION
+#=============================================================================
+
+Invoke-OSDCloud
+#Restart from WinPE
+Write-Host -ForegroundColor Cyan 'Restarting in 20 seconds!'
+Start-Sleep -Seconds 20
+wpeutil reboot
+
+#=============================================================================
+#endregion
+#=============================================================================
