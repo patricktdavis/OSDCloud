1032c781adc7517c2c92150c5fd72da60f86e913
patricktdavis <patricktdavis@outlook.com>
2021-07-26 16:08:58 -0500

    Update OSDCloud-Hennepin.ps1

--- OSDCloud-Hennepin.ps1
+++ OSDCloud-Hennepin.ps1
@@ -5,6 +5,7 @@
 <#
 .SYNOPSIS
 Runs a menu on OSDCloud allowing techs to deploy a machine with Chosen Enterprise image
+
 .EXAMPLE
 PS C:\> OSDCloud-Hennepin.ps1
 #>
@@ -13,19 +14,24 @@ PS C:\> OSDCloud-Hennepin.ps1
 #endregion
 #=============================================================================
 
+$OS = 'Windows 10 Enterprise 20H2'
+$Serial = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber
+
 #=============================================================================
 #region FUNCTIONS
 #=============================================================================
-
-function Invoke-OSDCloud {
+function NewBoxHD {
     <#
     .SYNOPSIS
     Synopsis
+
     .EXAMPLE
-    Invoke-OSDCloud
+    NewBox
+
     .INPUTS
     None
-    You cannot pipe objects to Invoke-OSDCloud.
+    You cannot pipe objects to NewBox.
+
     .OUTPUTS
     None
     The cmdlet does not return any output.
@@ -34,13 +40,220 @@ function Invoke-OSDCloud {
     [CmdletBinding()]
     Param()
 
-    if ((Get-MyComputerModel) -match 'Virtual') {
-        Write-Host -ForegroundColor Cyan 'Setting Display Resolution to 1600x'
-        Set-DisRes 1600
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.Application]::EnableVisualStyles()
+
+    $PopupBox = New-Object system.Windows.Forms.Form
+    $PopupBox.ClientSize = New-Object System.Drawing.Point(1300,550)
+    $PopupBox.text = 'Hennepin County SSD Team'
+    $PopupBox.TopMost = $true
+    $PopupBox.Controlbox = $false
+
+    $Title = New-Object system.Windows.Forms.Label
+    $Title.text = 'Welcome to Hennepin County Imaging'
+    $Title.AutoSize = $true
+    $Title.width = 25
+    $Title.height = 10
+    $Title.location = New-Object System.Drawing.Point(26,34)
+    $Title.Font = New-Object System.Drawing.Font('Segoe UI',28,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+
+    $OSDescription = New-Object system.Windows.Forms.Label
+    $OSDescription.text = 'The following Operating System will be installed: '
+    $OSDescription.AutoSize = $true
+    $OSDescription.width = 25
+    $OSDescription.height = 10
+    $OSDescription.location = New-Object System.Drawing.Point(43,119)
+    $OSDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
+
+    $OperatingSystemLabel = New-Object system.Windows.Forms.Label
+    $OperatingSystemLabel.text = "$OS"
+    $OperatingSystemLabel.AutoSize = $true
+    $OperatingSystemLabel.width = 25
+    $OperatingSystemLabel.height = 10
+    $OperatingSystemLabel.location = New-Object System.Drawing.Point(41,163)
+    $OperatingSystemLabel.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+
+    $SerialDescription = New-Object system.Windows.Forms.Label
+    $SerialDescription.text = 'This Machines Serial Number is:'
+    $SerialDescription.AutoSize = $true
+    $SerialDescription.width = 25
+    $SerialDescription.height = 10
+    $SerialDescription.location = New-Object System.Drawing.Point(41,244)
+    $SerialDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
+
+    $SerialLabel = New-Object system.Windows.Forms.Label
+    $SerialLabel.text = "$Serial"
+    $SerialLabel.AutoSize = $true
+    $SerialLabel.width = 25
+    $SerialLabel.height = 10
+    $SerialLabel.location = New-Object System.Drawing.Point(43,289)
+    $SerialLabel.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+
+    $MakeASelection = New-Object system.Windows.Forms.Label
+    $MakeASelection.text = 'Please make a selection below:'
+    $MakeASelection.AutoSize = $true
+    $MakeASelection.width = 25
+    $MakeASelection.height = 10
+    $MakeASelection.location = New-Object System.Drawing.Point(39,358)
+    $MakeASelection.Font = New-Object System.Drawing.Font('Segoe UI',20)
+
+    $ExitButton = New-Object system.Windows.Forms.Button
+    $ExitButton.text = 'Exit'
+    $ExitButton.width = 257
+    $ExitButton.height = 43
+    $ExitButton.location = New-Object System.Drawing.Point(43,466)
+    $ExitButton.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+    $ExitButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+
+    $InstallWindows = New-Object system.Windows.Forms.Button
+    $InstallWindows.text = 'Install Windows 10'
+    $InstallWindows.width = 500
+    $InstallWindows.height = 43
+    $InstallWindows.location = New-Object System.Drawing.Point(433,466)
+    $InstallWindows.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold -bor [System.Drawing.FontStyle]::Underline))
+    $InstallWindows.DialogResult = [System.Windows.Forms.DialogResult]::OK
+
+    $PopupBox.controls.AddRange(@($InstallWindows,$Title,$OSDescription,$OperatingSystemLabel,$SerialDescription,$SerialLabel,$MakeASelection,$ExitButton))
+
+    If ($Result -eq [System.Windows.Forms.DialogResult]::OK) {
+        #Installing latest OSD Content
+        Write-Host -ForegroundColor Cyan 'Updating OSD PowerShell Module'
+        Install-Module OSD -Force
+
+        Write-Host -ForegroundColor Cyan 'Importing OSD PowerShell Module'
+        Import-Module OSD -Force
+        Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+
     }
+}
 
-    $Serial = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber
+function NewBox4k {
+    <#
+    .SYNOPSIS
+    Synopsis
 
+    .EXAMPLE
+    NewBox
+
+    .INPUTS
+    None
+    You cannot pipe objects to NewBox.
+
+    .OUTPUTS
+    None
+    The cmdlet does not return any output.
+    #>
+
+    [CmdletBinding()]
+    Param()
+
+
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.Application]::EnableVisualStyles()
+
+    $PopupBox = New-Object system.Windows.Forms.Form
+    $PopupBox.ClientSize = New-Object System.Drawing.Point(2400,1450)
+    $PopupBox.text = 'Hennepin County SSD Team'
+    $PopupBox.TopMost = $true
+    $PopupBox.Controlbox = $false
+
+    $Title = New-Object system.Windows.Forms.Label
+    $Title.text = 'Welcome to Hennepin County Imaging'
+    $Title.AutoSize = $true
+    $Title.width = 25
+    $Title.height = 10
+    $Title.location = New-Object System.Drawing.Point(26,34)
+    $Title.Font = New-Object System.Drawing.Font('Segoe UI',28,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+
+    $OSDescription = New-Object system.Windows.Forms.Label
+    $OSDescription.text = 'The following Operating System will be installed: '
+    $OSDescription.AutoSize = $true
+    $OSDescription.width = 25
+    $OSDescription.height = 10
+    $OSDescription.location = New-Object System.Drawing.Point(43,300)
+    $OSDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
+
+    $OperatingSystemLabel = New-Object system.Windows.Forms.Label
+    $OperatingSystemLabel.text = "$OS"
+    $OperatingSystemLabel.AutoSize = $true
+    $OperatingSystemLabel.width = 25
+    $OperatingSystemLabel.height = 10
+    $OperatingSystemLabel.location = New-Object System.Drawing.Point(41,400)
+    $OperatingSystemLabel.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+
+    $SerialDescription = New-Object system.Windows.Forms.Label
+    $SerialDescription.text = 'This Machines Serial Number is:'
+    $SerialDescription.AutoSize = $true
+    $SerialDescription.width = 25
+    $SerialDescription.height = 10
+    $SerialDescription.location = New-Object System.Drawing.Point(41,600)
+    $SerialDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
+
+    $SerialLabel = New-Object system.Windows.Forms.Label
+    $SerialLabel.text = "$Serial"
+    $SerialLabel.AutoSize = $true
+    $SerialLabel.width = 25
+    $SerialLabel.height = 10
+    $SerialLabel.location = New-Object System.Drawing.Point(43,800)
+    $SerialLabel.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+
+    $MakeASelection = New-Object system.Windows.Forms.Label
+    $MakeASelection.text = 'Please make a selection below:'
+    $MakeASelection.AutoSize = $true
+    $MakeASelection.width = 25
+    $MakeASelection.height = 10
+    $MakeASelection.location = New-Object System.Drawing.Point(39,1000)
+    $MakeASelection.Font = New-Object System.Drawing.Font('Segoe UI',20)
+
+    $ExitButton = New-Object system.Windows.Forms.Button
+    $ExitButton.text = 'Exit'
+    $ExitButton.width = 257
+    $ExitButton.height = 100
+    $ExitButton.location = New-Object System.Drawing.Point(43,1200)
+    $ExitButton.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+    $ExitButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+
+    $InstallWindows = New-Object system.Windows.Forms.Button
+    $InstallWindows.text = 'Install Windows 10'
+    $InstallWindows.width = 1000
+    $InstallWindows.height = 100
+    $InstallWindows.location = New-Object System.Drawing.Point(433,1200)
+    $InstallWindows.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold -bor [System.Drawing.FontStyle]::Underline))
+    $InstallWindows.DialogResult = [System.Windows.Forms.DialogResult]::OK
+
+    $PopupBox.controls.AddRange(@($InstallWindows,$Title,$OSDescription,$OperatingSystemLabel,$SerialDescription,$SerialLabel,$MakeASelection,$ExitButton))
+
+    If ($Result -eq [System.Windows.Forms.DialogResult]::OK) {
+        #Installing latest OSD Content
+        Write-Host -ForegroundColor Cyan 'Updating OSD PowerShell Module'
+        Install-Module OSD -Force
+
+        Write-Host -ForegroundColor Cyan 'Importing OSD PowerShell Module'
+        Import-Module OSD -Force
+        Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+
+    }
+}
+
+function NewBox {
+    <#
+    .SYNOPSIS
+    Synopsis
+
+    .EXAMPLE
+    NewBox
+
+    .INPUTS
+    None
+    You cannot pipe objects to NewBox.
+
+    .OUTPUTS
+    None
+    The cmdlet does not return any output.
+    #>
+
+    [CmdletBinding()]
+    Param()
     Add-Type -AssemblyName System.Windows.Forms
     [System.Windows.Forms.Application]::EnableVisualStyles()
 
@@ -67,7 +280,7 @@ function Invoke-OSDCloud {
     $OSDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
 
     $OperatingSystemLabel = New-Object system.Windows.Forms.Label
-    $OperatingSystemLabel.text = 'Windows 10 Enterprise 20H2'
+    $OperatingSystemLabel.text = "$OS"
     $OperatingSystemLabel.AutoSize = $true
     $OperatingSystemLabel.width = 25
     $OperatingSystemLabel.height = 10
@@ -126,6 +339,47 @@ function Invoke-OSDCloud {
         Import-Module OSD -Force
         Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
     }
+}
+
+function Invoke-OSDCloud {
+    <#
+    .SYNOPSIS
+    Synopsis
+
+    .EXAMPLE
+    Invoke-OSDCloud
+
+    .INPUTS
+    None
+    You cannot pipe objects to Invoke-OSDCloud.
+
+    .OUTPUTS
+    None
+    The cmdlet does not return any output.
+    #>
+
+    [CmdletBinding()]
+    Param()
+
+    if ((Get-MyComputerModel) -match 'Virtual') {
+        Write-Host -ForegroundColor Cyan 'Setting Display Resolution to 1600x'
+        Set-DisRes 1600
+    }
+
+    Add-Type -AssemblyName System.Windows.Forms
+    $Monitors = [System.Windows.Forms.Screen]::AllScreens
+    foreach ($Monitor in $Monitors) {
+        $Width = $Monitor.bounds.Width
+    }
+
+    If ($Width = '1920') {
+        NewBoxHD
+    }
+
+    elseif ($Width = '3840') {
+
+    } { NewBox4k }
+    else { NewBox }
 }
 
 #=============================================================================
