17287a93982856b4c0e37d339d7cb010b23a5602
patricktdavis <patricktdavis@outlook.com>
2021-09-15 21:04:59 -0500

    Update OSDCloud-Hennepin.ps1

--- OSDCloud-Hennepin.ps1
+++ OSDCloud-Hennepin.ps1
@@ -128,11 +128,11 @@ function Invoke-NewBoxHD {
         $Selection = $OSSelection.SelectedItem
 
         if ($Selection -eq 'Windows 10 v20H2') {
-            return $WIN10 = 'v20H2'
+            return "$WIN10 = 'v20H2'"
         }
 
         if ($Selection -eq 'HCSO Windows 10 v1909') {
-            return $WIN10 = 'v1909'
+            return "$WIN10 = 'v1909'"
         }
     }
 }
@@ -247,11 +247,11 @@ function Invoke-NewBox4k {
         $Selection = $OSSelection.SelectedItem
 
         if ($Selection -eq 'Windows 10 v20H2') {
-            return $WIN10 = 'v20H2'
+            return "$WIN10 = 'v20H2'"
         }
 
         if ($Selection -eq 'HCSO Windows 10 v1909') {
-            return $WIN10 = 'v1909'
+            return "$WIN10 = 'v1909'"
         }
     }
 }
@@ -306,11 +306,11 @@ function Invoke-OSDCloud {
 
 Invoke-OSDCloud
 If ($WIN10 -eq 'v20H2') {
-    Write-Host -ForegroundColor Cyan 'v20H2'
+    Write-Host -ForegroundColor Cyan 'Selected v20H2'
     $Global:Command = 'Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI'
     Start-Process powershell -ArgumentList "-noexit -command invoke-command -scriptblock {$Global:Command}" -Wait
 } Elseif ($WIN10 -eq 'v1909') {
-    Write-Host -ForegroundColor Cyan 'v1909'
+    Write-Host -ForegroundColor Cyan 'Selected v1909'
     $Global:Command = 'Start-OSDCloud -OSLanguage en-us -OSBuild 1909 -OSEdition Enterprise -ZTI'
     Start-Process powershell -ArgumentList "-noexit -command invoke-command -scriptblock {$Global:Command}" -Wait
 }
